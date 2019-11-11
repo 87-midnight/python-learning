cced2043821466f8cb01d6e6ada2d0d4e9da0012
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-11 21:31:08 +0800

    集合，字典的使用

---
+++ basic_test.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+from list_test import _list_,_map_
+
+
+def hello():
+    _str = input("input something:")
+    print("%s" % _str)
+    _list_()
+    _map_()
+
+
+hello()
---
+++ list_test.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+
+
+def _list_():
+    _array = [1, 2, 3, 4, 5]
+    print(_array)
+
+
+def _map_():
+    _map = {1: 'hello', "2": 220}
+    print("遍历map:%s" % _map)
+    if _map.__contains__("2"):
+        print(_map.get("2"))
+    for key in _map.keys():
+        print("[%s,%s]" % (key, _map.get(key)))
