3d6f0ef58450633335d26b1f94ce4bff71e3a50b
AnnieCattice <AnnieCattice@hotmail.com>
2019-11-13 10:10:54 +0800

    tcp测试

---
+++ packages/socket/tcp_server.py
@@ -0,0 +1,30 @@
+# -*- coding:utf8 -*-
+import socket
+import threading
+import time
+
+
+def tcplink(sock, addr):
+    print("accept new connection from %s:%s..." % addr)
+    sock.send("Welcom!".encode())
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if data == 'exit' or not data:
+            break
+        sock.send("hello: ".encode() + data)
+    sock.close()
+    print("Connection from %s:%s closed." % addr)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建一个基于ipv4 的TCP协议的socket
+
+s.bind(('127.0.0.1', 9999))  # 监听端口
+
+s.listen(5)
+print("Waiting for connection......")
+
+while True:
+    sock, addr = s.accept()
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
